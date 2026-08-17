4ce7128d4766a8021afc36aae07fe3d9ce7ca7b0
Ducanh2005HUST <ducanheya@gmail.com>
2026-08-17 21:51:20 +0700

    init project

---
+++ src/test/java/com/sunasterisk/sunbooking/SunBookingToursApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sunasterisk.sunbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunBookingToursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
